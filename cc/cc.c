21095ca15289024074ae839ce5fceb3ef338e621
tsubasasugawara <sugawara1740@gmail.com>
2023-01-01 04:30:43 +0900

    エラーメッセージを改良

--- cc/cc.c
+++ cc/cc.c
@@ -26,12 +26,19 @@ struct Token
 // 現在着目しているトークン
 Token *token;
 
-// エラーを報告するための関数
-// printfと同じ引数を取る
-void error(char *fmt, ...)
+// 入力プログラム
+char *user_input;
+
+// エラー箇所を報告する
+void error_at(char *loc, char *fmt, ...)
 {
     va_list ap;
     va_start(ap, fmt);
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, " "); // pos個の空白を出力
+    fprintf(stderr, "^ ");
     vfprintf(stderr, fmt, ap);
     fprintf(stderr, "\n");
     exit(1);
@@ -52,7 +59,7 @@ bool consume(char op)
 void expect(char op)
 {
     if (token->kind != TK_RESERVED || token->str[0] != op)
-        error("It is not '%c'.", op);
+        error_at(token->str, "It is not '%c'.");
     token = token->next;
 }
 
@@ -61,7 +68,7 @@ void expect(char op)
 int expect_number()
 {
     if (token->kind != TK_NUM)
-        error("It is not a number.");
+        error_at(token->str, "It is not a number.");
     int val = token->val;
     token = token->next;
     return val;
@@ -111,7 +118,7 @@ Token *tokenize(char *p)
             continue;
         }
 
-        error("Cannot be tokenized.");
+        error_at(token->str, "Cannot be tokenized.");
     }
 
     new_token(TK_EOF, cur, p);
@@ -125,6 +132,8 @@ int main(int argc, char **argv)
         fprintf(stderr, "The number of arguments is incorrect.\n");
         return 1;
     }
+
+    user_input = argv[1];
 
     // トークナイズ
     token = tokenize(argv[1]);
