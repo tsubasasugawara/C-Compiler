f27c13f03f7cc9e215449a4ba0539eb2d36b3c5c
tsubasasugawara <sugawara1740@gmail.com>
2023-01-01 18:05:25 +0900

    単項プラスと単項マイナス

--- cc/cc.c
+++ cc/cc.c
@@ -175,7 +175,9 @@ Node *new_node_num(int val)
     return node;
 }
 
+Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
 
 Node *expr()
@@ -195,17 +197,26 @@ Node *expr()
 
 Node *mul()
 {
-    Node *node = primary();
+    Node *node = unary();
 
     for (;;)
     {
         if (consume('*'))
-            node = new_node(ND_MUL, node, primary());
+            node = new_node(ND_MUL, node, unary());
         else if (consume('/'))
-            node = new_node(ND_DIV, node, primary());
+            node = new_node(ND_DIV, node, unary());
         else
             return node;
     }
+}
+
+Node *unary()
+{
+    if (consume('+'))
+        return unary();
+    if (consume('-'))
+        return new_node(ND_SUB, new_node_num(0), unary());
+    return primary();
 }
 
 Node *primary()
